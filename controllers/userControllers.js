02749782e9e7c11a06947a9773df5341cfe9077d
Govind <you@example.com>
2024-08-05 13:10:05 +0530

    updating_code

---
+++ controllers/userControllers.js
@@ -0,0 +1,71 @@
+const requestIp = require('request-ip');
+const Admin = require('../models/User');
+const AdminToken = require("../models/Usertoken")
+const { validPassword, generateSalt, hashPassword, generateJWT } = require('../helpers/auth');
+const responseManagement = require('../helpers/responseManagement');
+const httpStatus = require('http-status-codes');
+module.exports.RegisterUser=async(req,res)=>{
+    try {
+        const { name, email, password,Department,Divison } = req.body;
+
+        const existingUser = await Admin.findOne({ email });
+        if (existingUser) {
+            return responseManagement.sendResponse(res, httpStatus.BAD_REQUEST, 'Email already in use');
+        }
+
+        const salt = generateSalt();
+        const hash = hashPassword(password, salt);
+
+        const newAdmin = new Admin({
+            name,
+            email,
+            salt,
+            hash,
+            ip: req.ip, 
+            Department,
+            Divison
+        });
+
+        await newAdmin.save();
+        responseManagement.sendResponse(res, httpStatus.CREATED, 'Admin registered successfully');
+    } catch (error) {
+        console.error(error);
+        responseManagement.sendResponse(res, httpStatus.INTERNAL_SERVER_ERROR, 'Internal server error');
+    }
+}
+module.exports.UserLogin=async(req,res)=>{
+    console.log(req.body)
+    try {
+        const { email, password } = req.body;
+        const req_ip = requestIp.getClientIp(req);
+        const user = await Admin.findOne({ email: email });
+
+        if (user && user.salt && user.hash) {
+            if (validPassword(password, user.hash, user.salt)) {
+                if (user.status) {
+                    if (user.ip) {
+                        const newObj = {
+                            id: user._id,
+                            name: user.name,
+                            email: user.email,
+                        }
+                        const token = generateJWT(newObj);
+                        await AdminToken.create({ admin_id: user._id, token, req_ip, user_agent: req.headers['user-agent'] });
+                        responseManagement.sendResponse(res, httpStatus.OK, 'Logged in successfully', { token,user });
+                    } else {
+                        responseManagement.sendResponse(res, httpStatus.BAD_REQUEST, 'Invalid IP address');
+                    }
+                } else {
+                    responseManagement.sendResponse(res, httpStatus.UNAUTHORIZED, 'User is inactive');
+                }
+            } else {
+                responseManagement.sendResponse(res, httpStatus.BAD_REQUEST, 'Invalid credentials');
+            }
+        } else {
+            responseManagement.sendResponse(res, httpStatus.BAD_REQUEST, 'Invalid credentials');
+        }
+    } catch (error) {
+        console.error(error);
+        responseManagement.sendResponse(res, httpStatus.INTERNAL_SERVER_ERROR, 'Internal server error');
+    }
+}
--- models/User.js
+++ models/User.js
@@ -1,17 +1,34 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
+const UserSchema = new Schema(
+  {
     name: { type: String, maxlength: 200 },
     email: { type: String, maxlength: 200, index: true },
     ip: { type: String, maxlength: 200 },
     hash: { type: String },
+
     salt: { type: String, maxlength: 200 },
+    Department:{
+      type:String
+    },
+    Divison:{
+      type:String
+    },
+    status:{
+      type:String,
+      enum:["true","false"]
+    },
+    phone_Number: {
+      type: String,
+    },
     status: { type: Boolean, default: true, index: true },
     createdAt: { type: Date, default: Date.now, index: true },
-    updatedAt: { type: Date, default: Date.now }
-}, { timestamps: true });
+    updatedAt: { type: Date, default: Date.now },
+  },
+  { timestamps: true }
+);
 
-const Admin = mongoose.model('user', UserSchema);
+const Admin = mongoose.model("user", UserSchema);
 
 module.exports = Admin;
