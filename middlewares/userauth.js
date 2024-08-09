6b3105167c6b88098f435adba0cdc2def6f5e4b7
Govind <you@example.com>
2024-08-09 10:06:59 +0530

    updating_code

---
+++ middlewares/userauth.js
@@ -0,0 +1,36 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+module.exports = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization || req.header('Authorization');
+        console.log(token);
+        
+        
+        if (!token) {
+            return res.status(401).json({ msg: "Unauthorized - No token provided" });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        if (!decoded) {
+            return res.status(401).json({ msg: "Invalid token" });
+        }
+
+        const admin = await User.findById(decoded.id).select('-password');
+
+        if (!admin) {
+            return res.status(404).json({ msg: "Admin not found" });
+        }
+
+        req.user = admin;
+        next();
+    } catch (error) {
+        console.error("Authentication error:", error);
+        if (error.name === "TokenExpiredError") {
+            res.status(401).json({ msg: "Unauthorized - Token has expired" });
+        } else {
+            res.status(500).json({ msg: "Internal Server Error" });
+        }
+    }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const auth=require("../middlewares/auth")
+const authuser=require("../middlewares/userauth")
 const {RegisterUser,UserLogin, AllUser, CreateReport, GetAllReports, getFilterByName,getFilterByComponent}=require("../controllers/userControllers")
 const {registerAdmin,adminLogin}=require("../controllers/adminController")
 const {upload}=require("../helpers/helper")
 router.post('/RegisterUser',auth,RegisterUser);
 router.post('/login',UserLogin)
-router.post('/AllUser',auth,AllUser)
-router.post('/createreport',upload.single("file"),auth,CreateReport)
+router.post('/AllUser',AllUser)
+router.post('/createreport',upload.single("file"),authuser,CreateReport)
 router.get('/AllReports',auth,GetAllReports);
 router.get('/filterByName',auth,getFilterByName)
 router.get('/filterbyCategory',auth,getFilterByComponent);
